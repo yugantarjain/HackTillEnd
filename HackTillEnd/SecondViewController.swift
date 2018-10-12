8d35b5e1d0371dce07531e49c906e56a9ce06b1c
yugantar jain <yugantarjain@yujs-macbook-pro.local>
2018-10-12 13:57:49 +0530

    alarm implemented

--- HackTillEnd/SecondViewController.swift
+++ HackTillEnd/SecondViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do
+        {
+            let audioPlayer = Bundle.main.path(forResource: "al", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPlayer!) as URL)
+        }
+        catch
+        {
+            //error
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +36,22 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var alarmButton: UIButton!
+    @IBAction func alarmStart(_ sender: UIButton) {
+        let state = player.isPlaying
+        if(state == false)
+        {
+            player.play()
+            player.numberOfLoops = -1
+            alarmButton.setTitle("Alarm(STOP)", for: .normal)
+        }
+        else
+        {
+            player.stop()
+            alarmButton.setTitle("Alarm(START)", for: .normal)
+        }
+        
+    }
+    
 }
 
